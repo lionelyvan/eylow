126d024b6fca1fdba14a0b1cf7f5785b6211d06d
lionelyvan <yvanlionelbian@gmail.com>
2026-06-02 19:07:28 +0200

    artist

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/artist-site.html", // Si tu l'as renommé en index.html, mets "/index.html" ici
+      },
+    ];
+  },
 };
 
 export default nextConfig;
